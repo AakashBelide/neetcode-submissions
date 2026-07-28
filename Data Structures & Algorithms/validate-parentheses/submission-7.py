b9fdf6b099ee97bdca50664673ee645a63137116
Belide Aakash <41340373+AakashBelide@users.noreply.github.com>
2026-07-28 15:44:47 -0400

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open_map = {"{", "[", "("}
+        close_map = { "}": "{", "]": "[", ")": "("}
+
+        stack = []
+
+        for op in s:
+            if op in open_map:
+                stack.append(op)
+            else:
+                if stack and stack[-1] == close_map[op]:
+                    stack.pop()
+                else:
+                    return False
+        
+        if len(stack)!=0:
+            return False
+        
+        return True
