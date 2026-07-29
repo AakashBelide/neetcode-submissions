5a86019f6c5c34f778ac0ebed8dcd31a639a01dc
Belide Aakash <41340373+AakashBelide@users.noreply.github.com>
2026-07-29 15:41:39 -0400

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,21 @@
+class MinStack:
+
+    def __init__(self):
+        self.min_stack1 = []
+        self.min_stack2 = []
+
+    def push(self, val: int) -> None:
+        self.min_stack1.append(val)
+        if (not self.min_stack2) or (self.min_stack2 and self.min_stack2[-1]>=val):
+            self.min_stack2.append(val)
+
+    def pop(self) -> None:
+        tmp = self.min_stack1.pop()
+        if self.min_stack2[-1]==tmp:
+            self.min_stack2.pop()
+
+    def top(self) -> int:
+        return self.min_stack1[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack2[-1]
